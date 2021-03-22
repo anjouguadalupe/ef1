1fe40235c1c39137eb4746ac1e7801716583599c
Guadalupe, Anjou Sustiguer <aguadalupe3@gatech.edu>
2021-03-22 15:05:50 -0400

    Copy All
    Copied All files

---
+++ Samurai.Data/SamuraiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samurai.Data
+{
+    public class SamuraiContext:DbContext
+    {
+        public DbSet<SamuraiApp.Domain.Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= 2LK4RC2; Initial Catalog=SamuraiAppData; Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Samurai.UI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Samurai.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace Samurai.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        private static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurais();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+        private static void AddSamurais()
+        {
+            var samurai = new SamuraiApp.Domain.Samurai { Name = "Anjou" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
